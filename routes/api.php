7b043c85b9d016d5c5048fdeca2dd09d4759975a
yan <yanlinnthor@hotmail.com>
2025-12-12 11:07:17 +0630

    feat: delete post, comment, tag resources

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/health', function () {
     return response()->json(['status' => 'OK']);
 });
-
-Route::apiResource('posts', App\Http\Controllers\Api\PostController::class);
-
-Route::apiResource('comments', App\Http\Controllers\Api\CommentController::class);
-
-Route::apiResource('tags', App\Http\Controllers\Api\TagController::class);
